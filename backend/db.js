f0cd834d191feed05d3a68ca897ff3a843cc7669
aman satyawani <amansatyawani10@gmail.com>
2024-10-09 12:53:43 +0530

    2nd backend commit

--- backend/db.js
+++ backend/db.js
@@ -1,37 +1,37 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://admin:%TboK15G3Zpx9T0VM%3E@cluster0.wvyjj.mongodb.net/"
-    );
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.log(error);
-  }
-};
+mongoose.connect(
+  "mongodb+srv://admin:%TboK15G3Zpx9T0VM%3E@cluster0.wvyjj.mongodb.net/paytm"
+);
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
   },
   password: {
     type: String,
-    minLenght: 6,
     required: true,
+    minLength: 6,
   },
   firstName: {
     type: String,
-    minLenght: 6,
     required: true,
+    trim: true,
+    maxLength: 50,
   },
   lastName: {
     type: String,
-    minLenght: 6,
     required: true,
+    trim: true,
+    maxLength: 50,
   },
 });
+
 const User = mongoose.model("User", userSchema);
 module.exports = {
   User,
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const mainRouter = require("./routes/index");
+app.use("/api/v1", mainRouter);
 
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
